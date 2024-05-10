bbeabe8154421c525f0a94c70801fcd749585c24
ShenanVindinu <shenanvindinu@gmail.com>
2024-05-10 18:44:02 +0530

    login removed

--- assets/js/crud-bootstrap.js
+++ assets/js/crud-bootstrap.js
@@ -1,17 +1,14 @@
-
-$("#loginPage").css({display: 'block'});
-$("#HomePage").css({display: 'none'});
+$("#HomePage").css({display: 'block'});
 $("#CustomerPage").css({display: 'none'});
 $("#ItemsPage").css({display: 'none'});
 $("#OrdersPage").css({display: 'none'});
-$("#Header").css({display: 'none'});
+$("#Header").css({display: 'block'});
 
 $("#GoToHome").eq(0).on('click', () => {
     $("#HomePage").css({display: 'block'});
     $("#CustomerPage").css({display: 'none'});
     $("#ItemsPage").css({display: 'none'});
     $("#OrdersPage").css({display: 'none'});
-    $("#Header").css({display: 'block'});
 });
 
 $("#GoToOrders").eq(0).on('click', () => {
@@ -19,7 +16,6 @@ $("#GoToOrders").eq(0).on('click', () => {
     $("#CustomerPage").css({display: 'none'});
     $("#ItemsPage").css({display: 'none'});
     $("#OrdersPage").css({display: 'block'});
-    $("#Header").css({display: 'block'});
 });
 
 $("#OrdersButtonInHome").eq(0).on('click', () => {
@@ -27,7 +23,6 @@ $("#OrdersButtonInHome").eq(0).on('click', () => {
     $("#CustomerPage").css({display: 'none'});
     $("#ItemsPage").css({display: 'none'});
     $("#OrdersPage").css({display: 'block'});
-    $("#Header").css({display: 'block'});
 });
 
 $("#GoToCustomers").eq(0).on('click', () => {
@@ -35,7 +30,6 @@ $("#GoToCustomers").eq(0).on('click', () => {
     $("#CustomerPage").css({display: 'block'});
     $("#ItemsPage").css({display: 'none'});
     $("#OrdersPage").css({display: 'none'});
-    $("#Header").css({display: 'block'});
 });
 
 $("#CustomerButtonInHome").eq(0).on('click', () => {
@@ -43,7 +37,6 @@ $("#CustomerButtonInHome").eq(0).on('click', () => {
     $("#CustomerPage").css({display: 'block'});
     $("#ItemsPage").css({display: 'none'});
     $("#OrdersPage").css({display: 'none'});
-    $("#Header").css({display: 'block'});
 });
 
 $("#GoToItems").eq(0).on('click', () => {
@@ -51,7 +44,6 @@ $("#GoToItems").eq(0).on('click', () => {
     $("#CustomerPage").css({display: 'none'});
     $("#ItemsPage").css({display: 'block'});
     $("#OrdersPage").css({display: 'none'});
-    $("#Header").css({display: 'block'});
 });
 
 $("#ItemsButtonInHome").eq(0).on('click', () => {
@@ -59,24 +51,5 @@ $("#ItemsButtonInHome").eq(0).on('click', () => {
     $("#CustomerPage").css({display: 'none'});
     $("#ItemsPage").css({display: 'block'});
     $("#OrdersPage").css({display: 'none'});
-    $("#Header").css({display: 'block'});
-});
-
-$("#loginButton").eq(0).on('click', () => {
-    $("#HomePage").css({display: 'block'});
-    $("#CustomerPage").css({display: 'none'});
-    $("#ItemsPage").css({display: 'none'});
-    $("#OrdersPage").css({display: 'none'});
-    $("#Header").css({display: 'block'});
-    $("#loginPage").css({display: 'none'});
-});
-
-$("#LogoutButton").eq(0).on('click', () => {
-    $("#HomePage").css({display: 'none'});
-    $("#CustomerPage").css({display: 'none'});
-    $("#ItemsPage").css({display: 'none'});
-    $("#OrdersPage").css({display: 'none'});
-    $("#Header").css({display: 'none'});
-    $("#loginPage").css({display: 'block'});
 });
 
