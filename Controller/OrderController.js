74119626ed695bbaf01a4c3b666e08e9181491f4
ShenanVindinu <shenanvindinu@gmail.com>
2024-05-14 12:32:13 +0530

    made a way to generate random order id

---
+++ Controller/OrderController.js
@@ -0,0 +1,21 @@
+function generateOrderID() {
+
+    let randomString = Math.random().toString(36).substring(2, 3);
+
+
+    let now = new Date();
+    let year = now.getFullYear().toString().substr(-2);
+    let month = ('0' + (now.getMonth() + 1)).slice(-2);
+    let day = ('0' + now.getDate()).slice(-2);
+
+
+    let orderID = "D" + year + month + randomString;
+
+    return orderID;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let orderID = generateOrderID();
+    console.log("Generated Order ID:", orderID);
+});
+
